f96d27d212ad622a413b2f4a4c185020a3207d24
fr <151407147+Cccccccccvvvgyf@users.noreply.github.com>
2025-02-04 21:23:32 +0530

    Delete half plot.R

--- half plot.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-# gg mean grammer of graphics
-# aes = aesthetic 
-geom_point()
-ggplot(data,aes(x,y)) + geom_bar()
-install.packages("reshape2")
-library(reshape2)
-ggplot(d1,aes(x=,y=))
-##melt function for reshape2 pakages
