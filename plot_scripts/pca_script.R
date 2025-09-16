b00526ad93960e1df21720f43be7dd355d0bc5ba
Farha <151407147+captainbioinfo@users.noreply.github.com>
2025-09-16 07:53:25 +0530

    Update and rename pca_script.R to plot_scripts/pca_script.R

--- plot_scripts/pca_script.R
+++ plot_scripts/pca_script.R
@@ -109,3 +109,4 @@ ggsave("pca_plot_for_ppt.png", plot = pca_plot, width = 10, height = 8, units = 
 # Save the plot as a PDF for printing or high-quality documents
 ggsave("pca_plot_for_pdf22.png", plot = pca_plot, width = 5400, height = 5400, units = "px", dpi = 600, bg = "white")
 ################
+
